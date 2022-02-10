9bda6b743e4d5578da6ae0a0d439e2d55056889d
Rimontoine <nardiotencha@gmail.com>
2022-02-10 15:09:07 +0100

    convert csv data into lists

--- bruteforce.py
+++ bruteforce.py
@@ -5,15 +5,21 @@ import csv
 def getData():
     while True:
         doc_path = input("Input the .csv file path here: ")
+
+        # Check if doc ends with .csv
         if doc_path.endswith('.csv'):
+
+            # Check if such file exist
             if os.path.isfile(doc_path):
                 with open(doc_path, 'r') as file:
                     field_names = ['name', 'price', 'profit']
                     d_reader = csv.DictReader(file)
                     headers = d_reader.fieldnames
+
+                    # Check if the csv header's elements are the right ones
                     if headers == field_names:
                         print("Data is readable, thanks for the input.")
-                        return file
+                        return doc_path
                     else:
                         print("Wrong kind of dataset.")
             else:
@@ -22,17 +28,40 @@ def getData():
             print("This isn't a .csv file.")
 
 
-# Maintenant que j'ai la file, il faut:
-# 1. Function qui créer toutes les combinaisons possibles, qui va être la base de la loop
-# 2. Passer cette génératon dans une function qui va faire le calcul des rentabilités
-# 3. Comparez si la valeur retourner et plus haute que la précédente, et en faire la valeur de référence si c'est le cas.
-def comboGenerator(csv_file):
-    print('ok')
+def csvRowIntoList(csv_doc_path):
+    with open(csv_doc_path, 'r') as file:
+        print('Collecting data ...')
+        file_read = csv.reader(file)
+        file_into_list = list(file_read)
+        del file_into_list[0]
+
+        # Not only does it take each row into it's own list
+        # It also add 2 element in each: 
+        # [3] is the profit after 2 year
+        # [4] is the new total of profit
+        for data in file_into_list:
+            cost_to_buy = int(data[1])
+            profit_percentage = int(data[2].removesuffix("%"))
+            profit = (cost_to_buy*profit_percentage)/100
+            new_total = cost_to_buy + profit
+            data.append(profit)
+            data.append(new_total)
+            print(data)
+        return file_into_list
+
+# 2. Essayer toutes les combinaisons
+# A chauqe génération de combinaisons ...
+# Si le total des prix > que 500, alors ça dégage.
+# Si le total des prix < que 500, ajouter les bénéfices.
+# Comparer les bénéfices du nouveau avec l'ancien meilleur,
+# puis garder le meilleurs.
+# Petit message comme quoi y'as un nouveau meilleur avec la somme.
 
 
 def brut():
-    data_table = getData()
-    comboGenerator(data_table)
+    csv_path = getData()
+    data_list = csvRowIntoList(csv_path)
+    # Prochaine fonction, essaye toutes les combinaisons
 
 
 if __name__ == "__main__":
