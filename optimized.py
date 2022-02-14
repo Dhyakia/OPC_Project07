52603b02d8a6c1c456a02e8f5783f6a929048dd0
Rimonteil <nardiotencha@gmail.com>
2022-02-14 16:19:57 +0100

    2 decimal format for benefit and profit

--- optimized.py
+++ optimized.py
@@ -1,6 +1,5 @@
 import os
 import csv
-import itertools
 
 
 def getData():
@@ -47,8 +46,10 @@ def csvRowIntoList(csv_doc_path):
             profit_percentage = float(data[2].removesuffix("%"))
             benefit = (cost_to_buy*profit_percentage)/100
             profit = cost_to_buy + benefit
-            data.append(benefit)
-            data.append(profit)
+            formated_benefit = "{:.2f}".format(benefit)
+            formated_profit = "{:.2f}".format(profit)
+            data.append(formated_benefit)
+            data.append(formated_profit)
         return file_into_list
 
 # Idées optimisation:
@@ -67,81 +68,13 @@ def csvRowIntoList(csv_doc_path):
 # Desc: Ne pas dupliquer ex: [1,2] & [2,1]
 # Solu: Utiliser ".combinations()" plutôt que ".permuations()"
 
-# TODO: 4. Créer des conditions permettant de trouver le "best";
-# Desc: Peut-être que je peux faire en sorte de trouver un élément qui
-# est strictement meilleur que les autres et qui doit, à 100% être ajouter ?
-# Solu: ???
-
-# TODO: 5. Enlever les données négatives
-
-
-def comboGenerator(list_of_actions):
-    print("Generating combination ...")
-    max_profit = 0
-
-    for i in range(1, len(list_of_actions)+1):
-        for subset in itertools.combinations(list_of_actions, i):
-            actions_names = []
-            actions_cost = 0
-            actions_benefit = 0
-            actions_profit = 0
-
-            for list in subset:
-                x = float(list[1])
-                actions_cost += x
-
-            if actions_cost < 500:
-                for list in subset:
-                    name = list[0]
-                    benefit = float(list[3])
-                    profit = float(list[4])
-
-                    actions_names.append(name)
-                    actions_benefit += benefit
-                    actions_profit += profit
-            else:
-                continue
-
-            if max_profit < actions_profit:
-                max_profit = actions_profit
-                output_new_best(
-                    actions_names,
-                    actions_cost,
-                    actions_benefit,
-                    actions_profit)
-            else:
-                continue
-
-
-def newBest(max_profit, action_benefit):
-
-    if action_benefit > max_profit:
-        max_profit = action_benefit
-        return max_profit
-    else:
-        max_profit = 0
-        return max_profit
-
-
-def output_new_best(names, cost, benefit, profit):
-    formated_cost = "{:.2f}".format(cost)
-    formated_benefit = "{:.2f}".format(benefit)
-    formated_profit = "{:.2f}".format(profit)
-
-    print("")
-    print("New best!!")
-    print("Best combo is: " + str(names)[1:-1])
-    print("For a buying cost of: " + str(formated_cost) + "€")
-    print("You get a benefit of: " + str(formated_benefit) + "€")
-    print("For a new total of: " + str(formated_profit) + "€")
-    print("")
+# TODO: 4. Enlever les données négatives
 
 
 def opti():
     csv_path = getData()
     data_list = csvRowIntoList(csv_path)
-    comboGenerator(data_list)
-    print("Done")
+    print(data_list)
 
 
 if __name__ == "__main__":
