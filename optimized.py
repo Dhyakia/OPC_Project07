6c198fcaeb96531216a45cfec687d6ff7adc9f6a
Rimonteil <nardiotencha@gmail.com>
2022-02-15 11:28:43 +0100

    Culling of the negatives and zeros

--- optimized.py
+++ optimized.py
@@ -1,5 +1,9 @@
 import os
 import csv
+import itertools
+
+
+MAX_COST = 500
 
 
 def getData():
@@ -33,30 +37,94 @@ def csvRowIntoList(csv_doc_path):
         print('Collecting data ...')
         file_read = csv.reader(file)
         file_into_list = list(file_read)
-
+        list_index = []
+        print("Number of value taken before sort:" + str(len(file_into_list)))
         # remove the header's element
         del file_into_list[0]
 
-        # Not only does it take each row into it's own list
-        # It also add 2 element in each:
+        # Add 2 parameter in each element:
         # [3] is the profit after 2 year
         # [4] is the new total of profit
         for data in file_into_list:
             cost_to_buy = float(data[1])
-            profit_percentage = float(data[2].removesuffix("%"))
-            benefit = (cost_to_buy*profit_percentage)/100
-            profit = cost_to_buy + benefit
-            formated_benefit = "{:.2f}".format(benefit)
-            formated_profit = "{:.2f}".format(profit)
-            data.append(formated_benefit)
-            data.append(formated_profit)
+            file_index = file_into_list.index(data)
+            if cost_to_buy > 0:
+                profit_percentage = float(data[2].removesuffix("%"))
+                benefit = (cost_to_buy*profit_percentage)/100
+                profit = cost_to_buy + benefit
+                formated_benefit = "{:.2f}".format(benefit)
+                formated_profit = "{:.2f}".format(profit)
+                data.append(formated_benefit)
+                data.append(formated_profit)
+            else:
+                list_index.append(file_index)
+
+        list_index.sort(reverse=True)
+        for index in list_index:
+            del file_into_list[index]
+
+        print("Number of value taken after sort:" + str(len(file_into_list)))
         return file_into_list
+
+
+def max_range_combos(list):
+    print("Calculating maximum range ...")
+    cost_list = []
+    range_counter = 0
+
+    for i in list:
+        cost_list.append(float(i[1]))
+
+    sorted_list = sorted(cost_list)
+    print(sorted_list)
+    count_counter = 0
+    for i in sorted_list:
+        count_counter = (count_counter + i)
+        range_counter += 1
+        if count_counter > MAX_COST:
+            return range_counter
+        else:
+            continue
+
+
+def comboGenerator(list_of_actions, max_range):
+    print("Generating combination ...")
+    max_profit = 0
+
+    for i in range(1, max_range):
+        for subset in itertools.combinations(list_of_actions, i):
+            actions_names = []
+            actions_cost = 0
+            actions_benefit = 0
+            actions_profit = 0
+
+            for list in subset:
+                x = float(list[1])
+                actions_cost += x
+
+            if actions_cost < MAX_COST:
+                for list in subset:
+                    name = list[0]
+                    benefit = float(list[3])
+                    profit = float(list[4])
+
+                    actions_names.append(name)
+                    actions_benefit += benefit
+                    actions_profit += profit
+            else:
+                continue
+
+            if max_profit < actions_profit:
+                max_profit = actions_profit
+            else:
+                continue
 
 # Idées optimisation:
 # TODO: 1. Utiliser de la récursion
 # Desc: Multiplier les fonctions pour gagner en temps contre de la mémoire
 # Solu: ???
 
+# DONE
 # TODO: 2. Enlever les combos trop haut;
 # Desc: Le script n'as aucunement besoin d'essayer toutes les combos.
 # Solu: Trier la liste par élément "cout", avec ".sort()".
@@ -64,17 +132,21 @@ def csvRowIntoList(csv_doc_path):
 # Une fois que la valeur > que 500, alors ...
 # (valeur-1) = la range du loop
 
+# DONE
 # TODO: 3. Ne pas faire de répétition;
 # Desc: Ne pas dupliquer ex: [1,2] & [2,1]
 # Solu: Utiliser ".combinations()" plutôt que ".permuations()"
 
-# TODO: 4. Enlever les données négatives
+# DONE
+# TODO: 4. Enlever les données négatives et les zéros
 
 
 def opti():
     csv_path = getData()
     data_list = csvRowIntoList(csv_path)
-    print(data_list)
+    max_range = max_range_combos(data_list)
+    comboGenerator(data_list, max_range)
+    print("Done")
 
 
 if __name__ == "__main__":
